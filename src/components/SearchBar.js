376bde4a01fbdf86cc7422fa3a760217b806b3b1
yogi <raoyogesh78@yahoo.in>
2021-01-13 00:46:35 +0100

    initialized state and onchange event for input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = { term: ''}
+    
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value })
+    }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input type="text" value={this.state.term} onChange={this.onInputChange} />
                     </div>
                 </form>    
             </div>
