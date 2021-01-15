428f28ac83b0f0d5c91a3b6baf7ed9553ca95d65
yogi <raoyogesh78@yahoo.in>
2021-01-15 07:30:26 +0100

    created react ref for images

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imageRef)
+    }
+
     render() {
         const { description, urls } = this.props.image;
 
         return (
             <div>
                 <img
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
