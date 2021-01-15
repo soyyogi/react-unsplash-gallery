635bf252241a22897b9f02bee753e3b2c6747f05
yogi <raoyogesh78@yahoo.in>
2021-01-15 05:01:49 +0100

    initialize state and set state for images

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+    state = { images: []};
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: {
                 query: term
@@ -12,13 +14,15 @@ class App extends React.Component {
                 Authorization: 'Client-ID GaisIEgGuPKfttIUTXz2L9uo9D8_EBXfOcYAHjw0U8w'
             }
         });
-        console.log(response.data.results)
+
+        this.setState({ images : response.data.results });
     }
     
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
