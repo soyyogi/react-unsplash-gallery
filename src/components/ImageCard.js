030177429d8562c9b7d2470f86d95649b13a2508
yogi <raoyogesh78@yahoo.in>
2021-01-15 07:43:51 +0100

    initialize state for spans and set spans for image grid row

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,11 +4,21 @@ class ImageCard extends React.Component {
     constructor(props){
         super(props);
 
+        this.state = { spans: 0 };
+
         this.imageRef = React.createRef();
     }
 
     componentDidMount() {
-        console.log(this.imageRef)
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 150);
+
+        this.setState({ spans });
     }
 
     render() {
